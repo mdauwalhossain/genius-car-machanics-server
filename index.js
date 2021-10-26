3d5e6f7c258c58727ebeb3f2201230e70717e91d
Md Auwal Hossain <mdauwalhossain2020@gmail.com>
2021-10-26 22:47:49 +0600

    PORT added

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require ('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+
+const cors = require('cors');
+require('dotenv').config()
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.imuoa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// console.log(uri);
+
+// --------------------------
+async function run(){
+    try{
+        await client.connect();
+        // console.log('connected to database');
+        const database = client.db('carMachanic');
+        const servicesCollection = database.collection('services');
+
+        // GET API
+        app.get('/services', async(req, res) =>{
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+        // GET Single service
+        app.get('/services/:id', async(req, res) =>{
+            const id =req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+        
+        
+        // POST API
+        app.post('/services', async(req, res) =>{
+            const service = req.body;
+            console.log('hit the post API', service);
+
+            // const service = {
+            //     "name": "ENGINE DIAGNOSTIC",
+            //     "price": "300",
+            //     "description": "Lorem ipsum dolor sit amet, consectetu radipisi cing elitBeatae autem aperiam nequ quaera molestias voluptatibus harum ametipsa.",
+            //     "img": "https://i.ibb.co/dGDkr4v/1.jpg"
+            // }
+            
+            
+            const result = await servicesCollection.insertOne(service);
+            console.log(result);
+            res.json (result)
+        });
+
+        // DELETE API
+        app.delete('/services/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
+
+
+    }
+    finally{
+        // awit client.close();
+    }
+}
+
+run().catch(console.dir);
+//-----------------------------
+
+app.get ('/', (req, res) =>{
+    res.send('Running Genius Server');
+});
+
+app.listen(port, () =>{
+    console.log('Running backend server properly.');
+})
